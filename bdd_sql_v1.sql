04625d5ec491bb7b7a3c6944475e907a4d3182d8
sae204 <98147394+sae204@users.noreply.github.com>
2022-01-21 08:42:05 +0100

    Update bdd_sql_v1.sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -1,4 +1,16 @@
-CREATE TABLE plats(
-    id INT
-    , PRIMARY KEY (id)
+CREATE TABLE typeVetements(
+   Id_typeVetements COUNTER,
+   libelle VARCHAR(50),
+   PRIMARY KEY(Id_typeVetements)
+);
+
+CREATE TABLE vetements(
+   Id_vetement COUNTER,
+   descriptif VARCHAR(100),
+   prixDeBase DECIMAL(15,2),
+   taille INT,
+   dateAchat DATE,
+   Id_typeVetements INT NOT NULL,
+   PRIMARY KEY(Id_vetement),
+   FOREIGN KEY(Id_typeVetements) REFERENCES typeVetements(Id_typeVetements)
 );
