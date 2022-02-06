637662fa166bd07e9fcac79c2b2026fe9c2549a6
sae204 <98147394+sae204@users.noreply.github.com>
2022-02-06 17:48:04 +0100

    Update bdd_sql_v1.sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -2,18 +2,17 @@ DROP TABLE IF EXISTS concerne;
 DROP TABLE IF EXISTS ligne_commande;
 DROP TABLE IF EXISTS est_posté_le;
 DROP TABLE IF EXISTS livre;
-DROP TABLE IF EXISTS est_contenu_dans;
 DROP TABLE IF EXISTS est_de;
 DROP TABLE IF EXISTS commande;
 DROP TABLE IF EXISTS commentaire;
 DROP TABLE IF EXISTS etat_commande;
+DROP TABLE IF EXISTS rempli_panier;
 DROP TABLE IF EXISTS utilisateur;
 DROP TABLE IF EXISTS vetement;
 DROP TABLE IF EXISTS note;
 DROP TABLE IF EXISTS date_heure;
 DROP TABLE IF EXISTS fournisseur;
 DROP TABLE IF EXISTS taille;
-DROP TABLE IF EXISTS panier;
 DROP TABLE IF EXISTS couleur;
 DROP TABLE IF EXISTS type_vetement;
 
@@ -27,12 +26,6 @@ CREATE TABLE couleur(
    id_couleur INT AUTO_INCREMENT,
    libelle_couleur VARCHAR(50),
    PRIMARY KEY(id_couleur)
-);
-
-CREATE TABLE panier(
-   id_panier INT AUTO_INCREMENT,
-   total DECIMAL(15,2),
-   PRIMARY KEY(id_panier)
 );
 
 CREATE TABLE taille(
@@ -60,24 +53,35 @@ CREATE TABLE note(
 
 CREATE TABLE vetement(
    id_vetement INT AUTO_INCREMENT,
-   nom_vetement VARCHAR(50),
-   descriptif VARCHAR(100),
+   nom_vetement VARCHAR(200),
+   descriptif VARCHAR(200),
    prix_de_base DECIMAL(15,2) NOT NULL,
    id_type_vetement INT NOT NULL,
+   image VARCHAR(100),
    PRIMARY KEY(id_vetement),
    FOREIGN KEY(id_type_vetement) REFERENCES type_vetement(id_type_vetement)
 );
 
+
+
 CREATE TABLE utilisateur(
    id_utilisateur INT AUTO_INCREMENT,
    username VARCHAR(50),
-   mot_de_passe VARCHAR(50),
-   est_actif LOGICAL,
-   adresse_mail VARCHAR(50),
-   role VARCHAR(255),
-   id_panier INT NOT NULL,
-   PRIMARY KEY(id_utilisateur),
-   FOREIGN KEY(id_panier) REFERENCES panier(id_panier)
+   mot_de_passe VARCHAR(200),
+   est_actif BIT,
+   adresse_mail VARCHAR(100),
+   role VARCHAR(20),
+   PRIMARY KEY(id_utilisateur)
+);
+
+
+CREATE TABLE rempli_panier(
+   id_vetement INT,
+   id_utilisateur INT,
+   quantite INT,
+   PRIMARY KEY(id_vetement, id_utilisateur),
+   FOREIGN KEY(id_vetement) REFERENCES vetement(id_vetement),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
 );
 
 CREATE TABLE etat_commande(
@@ -103,11 +107,8 @@ CREATE TABLE commande(
    id_commande INT AUTO_INCREMENT,
    date_commande DATE,
    id_etat_commande INT NOT NULL,
-   id_panier INT NOT NULL,
    PRIMARY KEY(id_commande),
-   UNIQUE(id_panier),
-   FOREIGN KEY(id_etat_commande) REFERENCES etat_commande(id_etat_commande),
-   FOREIGN KEY(id_panier) REFERENCES panier(id_panier)
+   FOREIGN KEY(id_etat_commande) REFERENCES etat_commande(id_etat_commande)
 );
 
 CREATE TABLE est_de(
@@ -119,15 +120,6 @@ CREATE TABLE est_de(
    FOREIGN KEY(id_vetement) REFERENCES vetement(id_vetement),
    FOREIGN KEY(id_couleur) REFERENCES couleur(id_couleur),
    FOREIGN KEY(id_taille) REFERENCES taille(id_taille)
-);
-
-CREATE TABLE est_contenu_dans(
-   id_vetement INT,
-   id_panier INT,
-   quantite_vetement VARCHAR(50),
-   PRIMARY KEY(id_vetement, id_panier),
-   FOREIGN KEY(id_vetement) REFERENCES vetement(id_vetement),
-   FOREIGN KEY(id_panier) REFERENCES panier(id_panier)
 );
 
 CREATE TABLE livre(
@@ -152,7 +144,7 @@ CREATE TABLE est_posté_le(
 CREATE TABLE ligne_commande(
    id_vetement INT,
    id_commande INT,
-   quantite_vetement_commande VARCHAR(50),
+   quantite_vetement_commande INT,
    PRIMARY KEY(id_vetement, id_commande),
    FOREIGN KEY(id_vetement) REFERENCES vetement(id_vetement),
    FOREIGN KEY(id_commande) REFERENCES commande(id_commande)
@@ -168,11 +160,11 @@ CREATE TABLE concerne(
 
 
 
-# user (id,username, password, role, est_actif, pseudo, email)
-# commande (id, date_achat, #user_id, #etat_id)
-# ligne_commande ( #commande_id , #article_id , prix_unit, quantite)
-# panier (id, date_ajout,  #user_id , #article_id, prix_unit, quantite)
-# etat (id, libelle )
+-- user (id,username, password, role, est_actif, pseudo, email)
+-- commande (id, date_achat, #user_id, #etat_id)
+-- ligne_commande ( #commande_id , #article_id , prix_unit, quantite)
+-- panier (id, date_ajout,  #user_id , #article_id, prix_unit, quantite)
+-- etat (id, libelle )
 
 #insert type_vetement
 INSERT INTO type_vetement (libelle_type_vetement) VALUES ("Pantalons");
@@ -223,13 +215,14 @@ INSERT INTO fournisseur (libelle_fournisseur) VALUES ("Hermès");
 INSERT INTO fournisseur (libelle_fournisseur) VALUES ("Gucci");
 INSERT INTO fournisseur (libelle_fournisseur) VALUES ("Zara");
 
-# insert admin
-INSERT INTO utilisateur (username,mot_de_passe,adresse_mail,role) VALUES ("admin1","sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a","admin1@gmail.com","admin");
-INSERT INTO utilisateur (username,mot_de_passe,adresse_mail,role) VALUES ("admin2","admin2","admin2@gmail.com","admin");
+
+-- insert utilisateur
+INSERT INTO utilisateur (username,mot_de_passe,adresse_mail,role) VALUES ("admin",'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a',"admin@gmail.com","admin");
+INSERT INTO utilisateur (username,mot_de_passe,adresse_mail,role) VALUES ("client","sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4","client@gmail.com","client");
+INSERT INTO utilisateur (username,mot_de_passe,adresse_mail,role) VALUES ("client2","sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3","client2@client2.fr","client");
 
 
-# insert vetement
-
+-- insert vetement
 INSERT INTO vetement (nom_vetement,descriptif,prix_de_base,id_type_vetement,image) VALUES
     ('T-shirt','T-shirt ?',12.5,6,'/static/images/tshirtbleu.jpg'),
     ('Pantalons','Pantalons ?',14.5,1,'/static/images/pantalon.jpg'),
@@ -251,11 +244,11 @@ INSERT INTO vetement (nom_vetement,descriptif,prix_de_base,id_type_vetement,imag
 
 
 
-# insert livre
-# INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (1,1,1,20);
-# INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (2,2,2,30);
-# INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (2,1,3,50);
-# INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (8,4,6,10);
+-- insert livre
+-- INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (1,1,1,20);
+-- INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (2,2,2,30);
+-- INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (2,1,3,50);
+-- INSERT INTO livre(id_vetement,id_fournisseur ,id_date_arrivage ,quantite_arrivage) VALUES (8,4,6,10);
 
 
 SELECT * FROM fournisseur;
@@ -263,13 +256,14 @@ SELECT * FROM taille;
 SELECT * FROM type_vetement;
 SELECT * FROM couleur;
 SELECT * FROM vetement;
+SELECT * FROM utilisateur;
 
 
 
-SELECT vetement.id_vetement,nom_vetement,prix_de_base,libelle_type_vetement,libelle_fournisseur,libelle_couleur
-             FROM vetement
-             LEFT JOIN est_de ON est_de.id_vetement = vetement.id_vetement
-             LEFT JOIN couleur ON couleur.id_couleur = est_de.id_couleur
-             LEFT JOIN type_vetement ON type_vetement.id_type_vetement = vetement.id_type_vetement
-             JOIN livre on vetement.id_vetement=livre.id_vetement
-             JOIN fournisseur on fournisseur.id_fournisseur=livre.id_fournisseur;
+-- SELECT vetement.id_vetement,nom_vetement,prix_de_base,libelle_type_vetement,libelle_fournisseur,libelle_couleur
+--              FROM vetement
+--              LEFT JOIN est_de ON est_de.id_vetement = vetement.id_vetement
+--              LEFT JOIN couleur ON couleur.id_couleur = est_de.id_couleur
+--              LEFT JOIN type_vetement ON type_vetement.id_type_vetement = vetement.id_type_vetement
+--              JOIN livre on vetement.id_vetement=livre.id_vetement
+--              JOIN fournisseur on fournisseur.id_fournisseur=livre.id_fournisseur;
